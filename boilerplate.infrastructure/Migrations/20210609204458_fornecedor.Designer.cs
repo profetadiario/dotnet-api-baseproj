8c246571b79ead6211dbaa280e8cf42c55846947
Enrique Lacerda <enriquelacerda.ifjf@gmail.com>
2021-06-09 17:45:33 -0300

    added migration fornecedor

--- boilerplate.infrastructure/Migrations/20210609204458_fornecedor.Designer.cs
+++ boilerplate.infrastructure/Migrations/20210609204458_fornecedor.Designer.cs
@@ -4,14 +4,16 @@ using Boilerplate.Infra.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Boilerplate.Presentation.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210609204458_fornecedor")]
+    partial class fornecedor
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ boilerplate.infrastructure/Migrations/20210609204458_fornecedor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Boilerplate.Presentation.Data.Migrations
+{
+    public partial class fornecedor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Fornecedor",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Nome = table.Column<string>(nullable: true),
+                    Vertical = table.Column<int>(nullable: false),
+                    Link = table.Column<string>(nullable: true),
+                    MantemHistorico = table.Column<bool>(nullable: false),
+                    Logo = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fornecedor", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Fornecedor");
+        }
+    }
+}
