870996e6a5c4cb866907a8230f051f4d78b4077f
Enrique Lacerda <enriquelacerda.ifjf@gmail.com>
2021-06-10 11:02:39 -0300

    atempt to solve migrations

--- boilerplate.infrastructure/Context/AppDbContext.cs
+++ boilerplate.infrastructure/Context/AppDbContext.cs
@@ -1,9 +1,6 @@
 ﻿using Boilerplate.Core.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Boilerplate.Infra.Context
 {
@@ -15,5 +12,5 @@ namespace Boilerplate.Infra.Context
         }
 
         public DbSet<Fornecedor> Fornecedor { get; set; }
-    }
+    } 
 }
