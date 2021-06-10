56de0895f6ad930f6bb5fe70b19ab971ad57f97e
Enrique Lacerda <enriquelacerda.ifjf@gmail.com>
2021-06-10 19:37:57 -0300

    still fixing migrations

--- boilerplate.infrastructure/Migrations/20210609204458_fornecedor.cs
+++ boilerplate.infrastructure/Migrations/20210609204458_fornecedor.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
 
 namespace Boilerplate.Presentation.Data.Migrations
 {
@@ -6,134 +7,27 @@ namespace Boilerplate.Presentation.Data.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropPrimaryKey(
-                name: "PK_Fornecedor",
-                table: "Fornecedor");
-
-            migrationBuilder.RenameTable(
+            migrationBuilder.CreateTable(
                 name: "Fornecedor",
-                newName: "TBL_FORNECEDOR");
-
-            migrationBuilder.RenameColumn(
-                name: "Vertical",
-                table: "TBL_FORNECEDOR",
-                newName: "VERTICAL");
-
-            migrationBuilder.RenameColumn(
-                name: "Nome",
-                table: "TBL_FORNECEDOR",
-                newName: "NOME");
-
-            migrationBuilder.RenameColumn(
-                name: "Link",
-                table: "TBL_FORNECEDOR",
-                newName: "LINK");
-
-            migrationBuilder.RenameColumn(
-                name: "Id",
-                table: "TBL_FORNECEDOR",
-                newName: "ID");
-
-            migrationBuilder.RenameColumn(
-                name: "MantemHistorico",
-                table: "TBL_FORNECEDOR",
-                newName: "HAS_HISTORICO");
-
-            migrationBuilder.AlterColumn<string>(
-                name: "NOME",
-                table: "TBL_FORNECEDOR",
-                type: "varchar(100)",
-                nullable: false,
-                oldClrType: typeof(string),
-                oldType: "nvarchar(max)",
-                oldNullable: true);
-
-            migrationBuilder.AlterColumn<string>(
-                name: "LINK",
-                table: "TBL_FORNECEDOR",
-                type: "varchar(200)",
-                nullable: false,
-                oldClrType: typeof(string),
-                oldType: "nvarchar(max)",
-                oldNullable: true);
-
-            migrationBuilder.AlterColumn<bool>(
-                name: "HAS_HISTORICO",
-                table: "TBL_FORNECEDOR",
-                type: "BIT",
-                nullable: false,
-                oldClrType: typeof(bool),
-                oldType: "bit");
-
-            migrationBuilder.AddPrimaryKey(
-                name: "PK_TBL_FORNECEDOR",
-                table: "TBL_FORNECEDOR",
-                column: "ID");
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Nome = table.Column<string>(nullable: true),
+                    Vertical = table.Column<int>(nullable: false),
+                    Link = table.Column<string>(nullable: true),
+                    MantemHistorico = table.Column<bool>(nullable: false),
+                    Logo = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fornecedor", x => x.Id);
+                });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropPrimaryKey(
-                name: "PK_TBL_FORNECEDOR",
-                table: "TBL_FORNECEDOR");
-
-            migrationBuilder.RenameTable(
-                name: "TBL_FORNECEDOR",
-                newName: "Fornecedor");
-
-            migrationBuilder.RenameColumn(
-                name: "VERTICAL",
-                table: "Fornecedor",
-                newName: "Vertical");
-
-            migrationBuilder.RenameColumn(
-                name: "NOME",
-                table: "Fornecedor",
-                newName: "Nome");
-
-            migrationBuilder.RenameColumn(
-                name: "LINK",
-                table: "Fornecedor",
-                newName: "Link");
-
-            migrationBuilder.RenameColumn(
-                name: "ID",
-                table: "Fornecedor",
-                newName: "Id");
-
-            migrationBuilder.RenameColumn(
-                name: "HAS_HISTORICO",
-                table: "Fornecedor",
-                newName: "MantemHistorico");
-
-            migrationBuilder.AlterColumn<string>(
-                name: "Nome",
-                table: "Fornecedor",
-                type: "nvarchar(max)",
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "varchar(100)");
-
-            migrationBuilder.AlterColumn<string>(
-                name: "Link",
-                table: "Fornecedor",
-                type: "nvarchar(max)",
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "varchar(200)");
-
-            migrationBuilder.AlterColumn<bool>(
-                name: "MantemHistorico",
-                table: "Fornecedor",
-                type: "bit",
-                nullable: false,
-                oldClrType: typeof(bool),
-                oldType: "boolean");
-
-            migrationBuilder.AddPrimaryKey(
-                name: "PK_Fornecedor",
-                table: "Fornecedor",
-                column: "Id");
+            migrationBuilder.DropTable(
+                name: "Fornecedor");
         }
     }
 }
